b2f14697447e0bc5e4e2428dbf97b4c4a01c3a30
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 20:25:25 +0100

    aded Divider component

---
+++ src/components/Divider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Divider = () => {
+  return (
+    <View style={{height: 1, backgroundColor: '#f1f1f1', width: '100%'}} />
+  );
+};
+
+export default Divider;
