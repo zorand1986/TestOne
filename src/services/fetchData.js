401e2e43478fc1cabc7fd9ee8752e16786f04951
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 20:31:55 +0100

    added props with default values to fetchData service

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const fetchData = (page = 1) => {
+const fetchData = (
+  page = 1,
+  since = '2011-12-19T15:28:46.493Z',
+  perPage = 30,
+) => {
   return axios.get('https://api.github.com/gists/public', {
     params: {
-      since: '2011-12-19T15:28:46.493Z',
-      per_page: 30,
-      page: page,
+      since,
+      per_page: perPage,
+      page,
     },
   });
 };
