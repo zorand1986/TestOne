e7dc0cf990d913719f1bc2640fd3d08c14c377da
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 16:49:06 +0100

    added ErrorComponent for handling missing data

---
+++ src/components/ErrorComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ErrorComponent = ({description}) => {
+  <View>
+    <Text>{description}</Text>
+  </View>;
+};
+
+export default ErrorComponent;
