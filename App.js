6117452756bd6f8f18472ba9b6e50ca4ebc5b132
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 16:21:10 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+import ListItem from './src/components/LIstItem';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ListItem />
+    </SafeAreaView>
+  );
+};
+
+export default App;
