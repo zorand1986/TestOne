51a414e31baada98b5d8adeb3c1183e7e02c0c17
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 19:18:04 +0100

    added FlatList to UsersList for displaying infinite list

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
 import ListItem from './ListItem';
+import ErrorComponent from './ErrorComponent';
+import fetchData from '../services/fetchData';
 
 const UsersList = () => {
-  return <ListItem />;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    setLoading(true);
+    fetchData(page).then(result => {
+      setData(result.data);
+      setLoading(false);
+    });
+  }, []);
+
+  const fetchMore = () => {
+    setPage(prevPage => prevPage + 1);
+    fetchData(page).then(result =>
+      setData(prevData => [...prevData, ...result.data]),
+    );
+  };
+
+  if (loading) {
+    return <Text>Loading</Text>;
+  }
+
+  if (!loading && !data) {
+    return <ErrorComponent description="No data." />;
+  }
+
+  // eslint-disable-next-line no-extra-boolean-cast
+  if (!!data) {
+    return (
+      <View>
+        <View style={{backgroundColor: '#c1c1c1'}}>
+          <Text style={{color: 'black', fontSize: 20, fontWeight: '600'}}>
+            Gists
+          </Text>
+        </View>
+        <FlatList
+          data={data}
+          onEndReached={fetchMore}
+          onEndReachedThreshold={0.5}
+          renderItem={({item}) => (
+            <ListItem avatar={item?.owner.avatar_url} fileName={item?.files} />
+          )}
+          keyExtractor={item => item.id}
+          initialNumToRender={30}
+        />
+      </View>
+    );
+  }
 };
 
 export default UsersList;
