e4cdd83a7021f5f496afd4cb74960e49aba22122
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 20:32:47 +0100

    removed getFileName to a seperate service

--- src/components/LIstItem.js
+++ src/components/LIstItem.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
-import Avatar from './Avatar';
 
 const ListItem = ({fileName, avatar}) => {
-  let parsedFileName;
-  function getFileName(obj) {
-    if (obj.filename) {
-      parsedFileName = obj.filename;
-    } else {
-      for (let key in obj) {
-        getFileName(obj[key]);
-      }
-    }
-  }
-  getFileName(fileName);
-  console.log({parsedFileName});
   return (
-    <View style={{flexDirection: 'row', height: 70, alignItems: 'center'}}>
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 70,
+        alignItems: 'center',
+        marginHorizontal: 12,
+      }}>
       <Image style={{width: 50, height: 50}} source={{uri: avatar}} />
-      <Text>{parsedFileName}</Text>
+      <Text style={{marginLeft: 12}}>{fileName}</Text>
     </View>
   );
 };
