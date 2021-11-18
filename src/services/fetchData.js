73f6645e02dabf4c58fcbfbf91bfdefeda8c2597
Zoran Damjanac <zorand1986@gmail.com>
2021-11-18 19:15:00 +0100

    updated fetchData service to dinamically update page

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const fetchData = async () => {
-  await axios.get('API: https://api.github.com/gists/public', {
+const fetchData = (page = 2) => {
+  return axios.get('https://api.github.com/gists/public', {
     params: {
-      since: 'YYYY-MM-DDTHH:MM:SSZ',
+      //   since: 'YYYY-MM-DDTHH:MM:SSZ',
       per_page: 30,
-      page: 2,
+      page: page,
     },
   });
 };
