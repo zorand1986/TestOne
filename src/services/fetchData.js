be44bb7769f86e3792a5b93e3cb71c71eb97f147
Zoran Damjanac <zorand1986@gmail.com>
2021-11-19 14:49:23 +0100

    added .env file to store sensitive data

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import {API_ENDPOINT} from '@env';
 
 const fetchData = (
   page = 1,
   since = '2011-12-19T15:28:46.493Z',
   perPage = 30,
 ) => {
-  return axios.get('https://api.github.com/gists/public', {
+  return axios.get(`${API_ENDPOINT}`, {
     params: {
       since,
       per_page: perPage,
